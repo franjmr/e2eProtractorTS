64197496ef8f8bb437a07e1b1a3ddfd82d73590e
franciscojmr <franciscojmr@meta4.com>
2020-03-23 15:32:17 +0100

    Get console log

--- __test__/pa.dossier.tab.salary.props.spec.ts
+++ __test__/pa.dossier.tab.salary.props.spec.ts
@@ -1,19 +1,22 @@
-import { browser } from "protractor";
+import { browser, logging } from "protractor";
 import { M4JsapiUtils } from "../__m4utils__/m4JsapiUtils";
 import { SessionStorageUtils } from "../__m4utils__/m4Utils";
 import { EmployeeInformationPage } from "../__pages__/employee_information.po";
 import visibilityJson from "../__mock__/visibility.json";
 import confJson from "../__mock__/conf.json";
 import confLocJson from "../__mock__/confLoc.json";
+import { Level } from "selenium-webdriver/lib/logging";
 
 describe("PA - Salary UI Properties Suite", function() {
 
     let m4JsApiUtils: M4JsapiUtils;
     let server: string;
     let empInfoPage: EmployeeInformationPage;
-    let jsonProps: string[] = ['x', 'y', 'width', 'height'];
+    let jsonProps: number[] = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
     
     beforeAll(async(done)=>{
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 120000;
+
         browser.ignoreSynchronization = true;
         server = "http://jonsnow.meta4.com:13020";
         let appUser = "JCM_ESS";
@@ -33,32 +36,34 @@ describe("PA - Salary UI Properties Suite", function() {
         done();
     });
     
-    jsonProps.forEach(name => {
-        describe(name, function() {
+    jsonProps.forEach(value => {
+        describe("PA - Salary UI probability '"+(value * 100)+"%' of properties to be false ", function() {
 
-            const changeInJson = (id: string, value: boolean, obj: object): void => {
+            const changeInJson = (id: string, obj: object): void => {
                 if(!obj){
                     return;
                 }
                 
                 for (const [k, v] of Object.entries(obj)) {
                   if (k === id) {
-                    obj[k] = value;
+                    obj[k] = Math.random() >= value;
                   } else if (typeof v === "object") {
-                    changeInJson(id, value, v);
+                    changeInJson(id, v);
                   }
                 }
             };
 
             beforeAll(async(done)=>{
-                changeInJson("showSection", false, visibilityJson)
-                changeInJson("visible", false, confJson)
-                changeInJson("visible", false, confLocJson)
+                changeInJson("showSection", visibilityJson);
+                changeInJson("visible", confJson);
+                changeInJson("visible", confLocJson);
 
                 await SessionStorageUtils.setItem("visibility_1001M4EMPLOYEE", JSON.stringify(visibilityJson));
                 await SessionStorageUtils.setItem("conf_1001M4EMPLOYEE", JSON.stringify(confJson));
                 await SessionStorageUtils.setItem("confLoc_1001M4EMPLOYEE", JSON.stringify(confLocJson));
                 
+                await browser.manage().logs().get('browser');
+
                 await empInfoPage.openAndWaitFor_PageIsReady();
                 
                 done();
@@ -75,15 +80,38 @@ describe("PA - Salary UI Properties Suite", function() {
                 expect(await containerTabs.isDisplayed()).toBeTruthy();
             });
             
-            it("should load Tab Salary",async()=>{
+            it("should load Tab Salary", async()=>{
                 const tabSalaryElem = await empInfoPage.empInfoTabSalary.getElement_Tab();
                 expect(await tabSalaryElem.isDisplayed()).toBeTruthy();
             });
             
-            it("should load Tab Salary Container",async()=>{
+            it("should load Tab Salary Container", async()=>{
                 await empInfoPage.empInfoTabSalary.clickOn_Tab();
                 const tabSalaryContainerElem = await empInfoPage.empInfoTabSalary.getElement_TabContainer();
                 expect(await tabSalaryContainerElem.isDisplayed()).toBeTruthy();
+            });
+
+            it("should not load console log errors",async()=>{
+                const browserLogs = await browser.manage().logs().get('browser');
+                const errors: string[] = [];
+
+                function getLogErrorMessage(value: logging.Entry){
+                    return new Promise( resolve =>{
+                        const _level = value.level;
+                        if(_level.name == Level.SEVERE.name){
+                            const _type = value.type;
+                            const _msg = value.message;
+                            console.log("LEVEL NAME "+_level.name + ". Type: "+_type+".Message: "+_msg);
+                            errors.push(_msg);
+                        }
+                        resolve(true);
+                    })
+                }
+                browserLogs.forEach(async (value: logging.Entry)=>{
+                    await getLogErrorMessage(value);
+                });
+
+                expect(errors.length).toEqual(0);
             });
         });
     });
