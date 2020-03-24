db0b7465d86750a542958d890dbec1051b489651
franciscojmr <franciscojmr@meta4.com>
2020-03-24 15:58:51 +0100

    Add reporter

--- conf.js
+++ conf.js
@@ -1,9 +1,7 @@
 exports.config = {
     SELENIUM_PROMISE_MANAGER: false,
     framework: 'jasmine',
-    capabilities: {
-      browserName: 'chrome'
-    },
+    capabilities: { browserName: 'chrome', chromeOptions: { args: [ "--headless", "--disable-gpu", "--window-size=1366,768"] } },
     suites: {
         props: '__dist__/**/*props*spec.js',
         ui: '__dist__/**/*ui*spec.js'
@@ -13,5 +11,25 @@ exports.config = {
     noGlobals: true,
     jasmineNodeOpts: {
         defaultTimeoutInterval: 120000
-    }
+    },
+    onPrepare: function() {
+
+        let jasmineReporters = require('jasmine-reporters');
+        let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+    
+        const junitReporter = new jasmineReporters.JUnitXmlReporter({
+            savePath: 'junit',
+            consolidateAll: false
+        });
+    
+        const specReporter = new SpecReporter({
+            spec: {
+              displayStacktrace: true,
+              displayDuration: true
+            }
+        });
+    
+        jasmine.getEnv().addReporter(junitReporter);
+        jasmine.getEnv().addReporter(specReporter);
+    } 
   };
