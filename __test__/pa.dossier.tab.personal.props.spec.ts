906d7ad76dbb8a40e862ab7cb714c105b7d06f0b
franciscojmr <franciscojmr@meta4.com>
2020-03-26 12:42:42 +0100

    Fix Personal Tab

--- __test__/pa.dossier.tab.personal.props.spec.ts
+++ __test__/pa.dossier.tab.personal.props.spec.ts
@@ -4,14 +4,14 @@ import { EmployeeInformationPage } from "../__pages__/employee_information.po";
 import visibilityJson from "../__mock__/visibility.json";
 import confJson from "../__mock__/conf.json";
 import confLocJson from "../__mock__/confLoc.json";
-import { browser } from "protractor";
+import { browser, element, by, ElementFinder } from "protractor";
 
 describe("PA - Personal UI Properties Suite", function() {
 
     let m4JsApiUtils: M4JsapiUtils;
     let empInfoPage: EmployeeInformationPage;
 
-    const falseProbability: number[] = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9];
+    const falseProbability: number[] = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
     
     beforeAll(async()=>{
         let server = "http://jonsnow.meta4.com:13020";
@@ -26,13 +26,15 @@ describe("PA - Personal UI Properties Suite", function() {
     });
 
     afterAll(async()=>{
-        await m4JsApiUtils.logoutPortal()
+        await m4JsApiUtils.logoutPortal();
     });
     
-    falseProbability.forEach(probability => {
+    falseProbability.forEach( async (probability) => {
         describe("PA - Personal UI probability '"+(probability * 100)+"%' of properties to be false ", function() {
             
             beforeAll(async()=>{
+                await SessionStorageUtils.clear();
+
                 JSON_Utils.setBooleanRandomValue("showTab", visibilityJson, probability);
                 JSON_Utils.setBooleanRandomValue("showSection", visibilityJson, probability);
                 JSON_Utils.setBooleanRandomValue("visible", confJson, probability);
@@ -44,40 +46,44 @@ describe("PA - Personal UI Properties Suite", function() {
                 await SessionStorageUtils.setItem("conf_1001M4EMPLOYEE", JSON.stringify(confJson));
                 await SessionStorageUtils.setItem("confLoc_1001M4EMPLOYEE", JSON.stringify(confLocJson));
                 
-                await BrowserUtil.cleanConsoleLog();
                 await empInfoPage.openAndWaitFor_PageIsReady();
+                await empInfoPage.refreshAndWaitFor_PageIsReady();
             });
 
-            it("Should load Employee Information Page", async()=>{
+            afterAll(async()=>{
+                await SessionStorageUtils.removeItem("visibility_1001M4EMPLOYEE");
+                await SessionStorageUtils.removeItem("conf_1001M4EMPLOYEE");
+                await SessionStorageUtils.removeItem("confLoc_1001M4EMPLOYEE");
+            });
+
+            it("Given I navigate to the Employee Information Page", async()=>{
                 const pageTitleElem = await empInfoPage.getPageTitle();
                 expect(await pageTitleElem.getText()).toEqual("EMPLOYEE PORTFOLIO");
             });
-            
-            it("Should display container Tabs", async()=>{
-                await empInfoPage.waitFor_TabsAreDisplayed();
-                const containerTabs = await empInfoPage.getContainerTabs();
-                expect(await containerTabs.isDisplayed()).toBeTruthy();
-            });
-            
-            it("should display Tab Salary", async()=>{
+                        
+            it("And the Personal Tab is displayed", async()=>{
                 const tabSalaryElem = await empInfoPage.empInfoTabPersonal.getElement_Tab();
                 expect(await tabSalaryElem.isDisplayed()).toBeTruthy();
             });
             
-            it("When user click on Tab Salary should load Tab Salary Container", async()=>{
-                await empInfoPage.empInfoTabPersonal.clickOn_Tab();
-                const tabSalaryCntnr = await empInfoPage.empInfoTabPersonal.getElement_TabContainer();
-                const tabSalaryCntnrOtherElem = await empInfoPage.empInfoTabPersonal.getElement_TabContainerOtherItems();
-                expect(await tabSalaryCntnr.isDisplayed()).toBeTruthy();
-                expect(await tabSalaryCntnrOtherElem.isDisplayed()).toBeTruthy();
+            it("When I click on the Personal Tab", async()=>{
+                await empInfoPage.empInfoTabPersonal.clickOn_Tab().then(()=>{
+                    expect(true).toBeTruthy()
+                }).catch(()=>{
+                    expect(false).toBeTruthy();
+                });
             });
 
-            it("should display button to update information", async()=>{
+            it("Then I should view the Personal Tab content", async()=>{
+                const tabSalaryCntnr = await empInfoPage.empInfoTabPersonal.getElement_TabContainer();
+                const tabSalaryCntnrOtherElem = await empInfoPage.empInfoTabPersonal.getElement_TabContainerOtherItems();
                 const buttonUpdateInformation = await empInfoPage.empInfoTabPersonal.getElement_ButtonUpdateInformation();
+                expect(await tabSalaryCntnr.isDisplayed()).toBeTruthy();
+                expect(await tabSalaryCntnrOtherElem.isDisplayed()).toBeTruthy();
                 expect(await buttonUpdateInformation.isDisplayed()).toBeTruthy();
             });
 
-            it("should interact with update data popup", async()=>{
+            it("And I should check all possible data modification options", async()=>{
                 await empInfoPage.empInfoTabPersonal.clickOn_ButtonUpdateInformation();
                 await empInfoPage.empInfoTabPersonal.popupUpdateInformation.waitForm_PopUpReady();
 
@@ -89,7 +95,6 @@ describe("PA - Personal UI Properties Suite", function() {
                         if(optIdx > 0){
                             await empInfoPage.empInfoTabPersonal.popupUpdateInformation.clickOn_WidgetSelectAssistantLeft();
                             await empInfoPage.empInfoTabPersonal.popupUpdateInformation.getElems_WidgetSelectListOptionsAssistantLeft()
-                            browser.sleep(500);
                         }
                         
                         const elemOption = elemOptions[optIdx];
@@ -97,25 +102,28 @@ describe("PA - Personal UI Properties Suite", function() {
                         await BrowserUtil.element_WaitUntilBeClickable(elemOption);
                         await elemOption.click();
                         await empInfoPage.waitForUntil_PageIsReady();
-                        
-                        await empInfoPage.empInfoTabPersonal.popupUpdateInformation.getElem_BlockActions().then(async()=>{
-                            const actions = await empInfoPage.empInfoTabPersonal.popupUpdateInformation.getElems_BlockActionContent();
-                            expect(actions.length).toBeGreaterThan(0);
+
+                        const popUpError = element(by.id("m4-error"));
+                        await popUpError.isDisplayed().then(async()=>{
+                            expect(await popUpError.getText()).toBeFalsy;
                         }).catch(async()=>{
-                            expect(true).toBeTruthy();
-                        });
-                        expect(true).toBeTruthy();
+                            const noActions = element(by.cssContainingText('#assistantLeftContent', 'No actions are available for these data'));
+                        
+                            await noActions.isDisplayed().then( async () =>{
+                                expect(true).toBeTruthy();
+                            }).catch(async ()=>{
+                                const actions = await empInfoPage.empInfoTabPersonal.popupUpdateInformation.getElems_BlockActionContent();
+                                expect(actions.length).toBeGreaterThan(0);
+                            });
+                        })
                     }catch(error){
-                        console.log("Pues me aburro!");
+                        console.warn("=== WARNING!- Loop Idx: "+optIdx+" - Error: "+error);
+                        continue;
+                    }finally{
+                        browser.sleep(500);
                     }
                 }
             });
-
-            it("should not load console log errors", async()=>{
-                const browserConsoleLogErrors = await BrowserUtil.getConsoleLogError();
-                expect(browserConsoleLogErrors.length).toEqual(0);
-            });
-
         });
     });
 });
